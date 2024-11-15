6bbdb56b7b7ad8565675813656068305b81f38b9
guille21-esp <guille-prza@gmail.com>
2024-11-15 16:37:42 +0100

    shut down server added, display server fixed

--- object_oriented_programming/C#/comp/comp.cs
+++ object_oriented_programming/C#/comp/comp.cs
@@ -30,7 +30,7 @@ public class Computer
         _IpAddress=ip;
     }
     
-    public void ShutDownComputer(string ip){  // computer turns off, ip not visible
+    public virtual void ShutDownComputer(string ip){  // computer turns off, ip not visible
         _IpAddress= null;
     }
     
@@ -52,7 +52,7 @@ public class Computer
         get {return this._Name;}
         set {this._Name= value;}
     }
-}
+}          //end of class computer
 
 public class Server : Computer{
     
@@ -74,8 +74,19 @@ public class Server : Computer{
         IpAddress= ip+ "10";
     }
     
-    public void shutDownServer(string ip){
-        IpAddress= null;
+    public override void ShutDownComputer(string ip){
+        Console.WriteLine("Are you sure you want to shut down the server? (y/n)");
+        string sure= Console.ReadLine();
+        if(sure == "y" || sure == "n"){
+            if(sure =="y"){
+                IpAddress=null;
+                Console.WriteLine("Server shut off");
+            }else{
+                Console.WriteLine("Server still on");
+            }
+        }else{
+            Console.WriteLine("invalid option");
+        }
     }
 }
 
@@ -87,13 +98,13 @@ public class Program
         Server server= new Server();
         server.BiosName= "DHCP";
         server.IpAddress= "1.1.1.1";
-        server.Name= "nameserver";
-        server.Type= "primary";
+        server.Name= "server_one";
+        server.Type= "net_server";
         
         net[0] = server;
         
         for(int i=1; i< net.Length; i++){
-            net[i]= new Computer( "bios_" + i.ToString(), "comp_"+i.ToString() );
+            net[i]= new Computer( "bios_" + i.ToString(), "computer_"+i.ToString() );
             net[i].StartComputer(randomIP());
         }
         
@@ -164,7 +175,7 @@ public class Program
                         }
                         break;
                     case 3:
-                        DisplayComputers();
+                        DisplayComputers(net);
                         break;
                     case 4: 
                         Console.WriteLine("Enter the IP address of the computer you want to shut down:");
@@ -202,7 +213,26 @@ public class Program
                         Console.WriteLine("not implemented yet");
                         break;
                     case 7:
-                        Console.WriteLine("not implemented yet");
+                        Console.WriteLine("Enter the IP address of the server you want to shut down:");
+                        string ipp_to_shut_down = Console.ReadLine();
+
+                        bool serverFound = false;
+                        foreach (var computer in net){
+                            if(computer is Server server){
+                                if (computer.IpAddress == ipp_to_shut_down)
+                                {
+                                    server.ShutDownComputer(ipp_to_shut_down); 
+                                    Console.WriteLine($"Server {server.Name} with IP {ipp_to_shut_down} has been shut down.");
+                                    serverFound = true;
+                                    break;
+                                }
+                            }else{
+                                Console.WriteLine("server not found");
+                            }
+                        }
+                        
+                        
+                        
                         break;
                     case 8:
                         quit=true;
@@ -213,15 +243,22 @@ public class Program
         }
     }
     
-    static void DisplayComputers(){
+    static void DisplayComputers(Computer[]net){
         Console.WriteLine("Computers in net: ");
-        foreach(var computer in net){
-            Type t = typeof(Computer);
-            if(t == typeof(Server)){
-                Console.WriteLine($"Name: {computer.Name}, BIOS: {computer.BiosName}, Type: {computer.Type}, IP: {(computer.IpAddress ?? "Offline")}");
+        foreach(var comp in net){
+            Type type1= comp.GetType();
+            string stringType = type1.ToString();
+            Console.Write("{0}\t\t{1}\t\t{2}\t\t{3}\t", comp.Name, comp.IpAddress, comp.BiosName, type1);
+            
+            
+            if(stringType == "Server"){
+                Server serv = (Server) comp;
+                System.Console.WriteLine("\t{0}", serv.Type);
             }else{
-                Console.WriteLine($"Name: {computer.Name}, BIOS: {computer.BiosName}, IP: {(computer.IpAddress ?? "Offline")}");
+                Console.WriteLine();
             }
+
+
             
         }
     }
